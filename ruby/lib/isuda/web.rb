dc914a6ffad8b0d524e01bf715a3f4be49ba1dd5
moyashipan <moyashipan@gmail.com>
2016-09-18 15:44:01 +0900

    よみやすく

--- ruby/lib/isuda/web.rb
+++ ruby/lib/isuda/web.rb
@@ -102,7 +102,7 @@ module Isuda
       def htmlify(content)
         chars = content.split('').uniq
         keywords = db.xquery(%| select name AS keyword from keyword where prefix in (?) |, chars).to_a
-        keywords.sort_by! { |k| - k.size }
+        keywords = keywords.sort_by { |k| k.size }.reverse
         pattern = keywords.map {|k| Regexp.escape(k[:keyword]) }.join('|')
 
         hash = Digest::MD5.hexdigest(content + pattern)
