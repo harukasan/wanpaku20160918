06184cda84d2009f19b9b80197288aed6a905fca
ik <ik@pixiv.com>
2016-09-18 15:11:07 +0900

    自作trie追加

--- ruby/lib/isuda/web.rb
+++ ruby/lib/isuda/web.rb
@@ -265,4 +265,37 @@ module Isuda
       JSON.generate(result: 'ok')
     end
   end
+
+  class Trie
+    attr_reader :root
+
+    def initialize
+      @root = Node.new(nil)
+    end
+
+    def add(word)
+      node = @root
+      word.chars { |ch|
+        unless node.next[ch] then
+          node.next[ch] = Node.new(node)
+        end
+        node = node.next[ch]
+      }
+      node.endflg = true
+    end
+  end
+
+  class Node
+    attr_accessor :next, :parent, :endflg
+    def initialize(parent)
+      # @now = char # 現在の文字
+      @next = {}; # 次のノードへの参照
+      @parent = parent # 親ノード(上まで辿って文字列を復元する)
+      @endflg = false;
+    end
+
+    def has_next?
+      @next != {}
+    end
+  end
 end
